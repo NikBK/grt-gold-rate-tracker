aabaee95224f8b150c99ce679816225eb931ef55
Nikhil BK <67317835+NikBK@users.noreply.github.com>
2025-05-10 20:15:16 +0530

    Update fetch_gold_rate.py

--- fetch_gold_rate.py
+++ fetch_gold_rate.py
@@ -2,9 +2,6 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
 
 # Set up the WebDriver (Assuming you have ChromeDriver installed)
 options = webdriver.ChromeOptions()
