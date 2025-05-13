f9aa960a46921c73b73635c656156096f152cdfe
Nikhil BK <67317835+NikBK@users.noreply.github.com>
2025-05-13 22:31:55 +0530

    Update generate_graph.py

--- generate_graph.py
+++ generate_graph.py
@@ -30,9 +30,9 @@ for i, (month, group) in enumerate(grouped):
 # Custom Y-axis ticks at 10-unit intervals
 min_rate = df['GRT Rate (approx)'].min()
 max_rate = df['GRT Rate (approx)'].max()
-start = int(np.floor(min_rate / 20) * 20)
-end = int(np.ceil(max_rate / 20) * 20)
-plt.yticks(np.arange(start, end + 1, 20))
+start = int(np.floor(min_rate / 10) * 10)
+end = int(np.ceil(max_rate / 10) * 10)
+plt.yticks(np.arange(start, end + 1, 10))
 
 plt.title("GRT Daily Gold Rate Trend by Month")
 plt.xlabel("Day of Month")
